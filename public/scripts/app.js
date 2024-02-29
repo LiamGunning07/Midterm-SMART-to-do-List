07a8156f8f831bc048f54f5d79c18a5bdeb5282f
Kai Wingfield <kaiwingfield17@gmail.com>
2024-02-29 09:26:32 -0800

    Add dropdown functionality, adjust html and css accordingly. Add app.js listeners

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,28 @@
 // Client facing scripts here
+$(document).ready(function() {
+
+  // event listener for submit button
+  $('#submitButton').on("sumbit", function(event) {
+    event.preventDefault();
+    const serializedItem = $("#form1").serialize();
+    $.ajax({
+      method: "POST",
+      url: "/",
+      data: serializedItem,
+      success: function(result) {
+        console.log("Item was posted successfully");
+        loadItems();
+      },
+      error: function(err) {
+        console.log("There was an error ",err);
+      }
+    });
+  })
+
+  // event listener for dropdown
+  $("#to-watch-bttn").click(function() {
+    $("#to-watch-drop").slideToggle();
+  });
+
+
+});
