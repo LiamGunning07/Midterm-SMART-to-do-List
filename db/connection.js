6420ad2b36bbc5df1b66e69f6658f6933d4c3d8d
Kai Wingfield <kaiwingfield17@gmail.com>
2024-02-29 11:58:25 -0800

    Add db connection, add comments, tweak css

--- db/connection.js
+++ db/connection.js
@@ -10,7 +10,19 @@ const dbParams = {
 };
 
 const db = new Pool(dbParams);
-
 db.connect();
-
 module.exports = db;
+// functions that use querys to leverage db info
+// export all query functions
+// in routs require db/connection.js
+
+
+//router.get("/properties", (req, res) => {
+//   database
+//     .getAllProperties(req.query, 20)
+//     .then((properties) => res.send({ properties }))
+//     .catch((e) => {
+//       console.error(e);
+//       res.send(e);
+//     });
+// });
